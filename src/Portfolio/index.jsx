c6f89b59610b331da7c8bf894f528acd13573c68
Patrick <ppduss@gmail.com>
2020-05-20 09:45:56 -0400

    Starting

---
+++ src/Portfolio/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './global.scss';
+
+const App = () => {
+
+  const Main = (
+   <div className="main_container">
+    <div className="website">website1</div>
+    <div className="website">website2</div>
+    <div className="website">website3</div>
+   </div>
+  )
+
+  return Main
+};
+
+export default App;
