9c75c848454a4368b718a9257c11496b989026b2
Patrick <ppduss@gmail.com>
2020-05-21 22:40:29 -0400

    Basic styling and structure complete

--- src/Portfolio/index.jsx
+++ src/Portfolio/index.jsx
@@ -11,7 +11,7 @@ const App = () => {
           <img className="face" src={face} alt="profile shot" />
           <div className="text_box">
             <div className="greetings">Hi, I'm Patrick</div>
-            <div className="iam">I'm a front-end developer</div>
+            <div className="iam">I'm a front-end developer living in beautiful Montréal  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa repellendus distinctio eum at iure veritatis ab tempore, tenetur, laborum, quidem dicta! Deleniti velit quis sapiente natus, maxime maiores sit aut.</div>
           </div>
         </div>
       </div>
@@ -20,23 +20,24 @@ const App = () => {
       <div id="transition3"></div>
       <div className="project_container">
         <div className="project" id="Mars">
-        <div id="marsBox">
-          <div className="title ">The Mars Weather Network</div>
-          <div className="subtitle">A X-day weather report using the latest data trasmitted from Mars using two NASA APIs.</div>
-        </div>
+          <div id="marsBox">
+            <div className="title ">The Mars Weather Network</div>
+            <div className="subtitle">A X-day weather report using the latest data trasmitted from Mars using two NASA APIs.</div>
+          </div>
         </div>
         <div className="project" id="Coffee">
-        <div id="coffeeBox">
-          <div className="title ">Coffee Prescriber</div>
-          <div className="subtitle">A playful app that helps gauge your happiness to productivity ratio as your drink coffee and wether it's wise to keep drinking.</div>
-        </div>
+          <div id="coffeeBox">
+           <div className="title ">Coffee Prescriber</div>
+            <div className="subtitle">A playful app that helps gauge your happiness to productivity ratio as your drink coffee and wether it's wise to keep drinking.</div>
+          </div>
         </div>
         <div className="project" id="Photo">
-        <div id="photoBox">
-          <div className="title "> Photography Portfolio</div>
-          <div className="subtitle">Blah blah blah, something something</div>
+          <div id="photoBox">
+            <div className="title "> Photography Portfolio</div>
+            <div className="subtitle">Blah blah blah, something something</div>
+          </div>
         </div>
-        </div>
+        <div className="footer">This is the footer</div>
       </div>
    </div>
   )
