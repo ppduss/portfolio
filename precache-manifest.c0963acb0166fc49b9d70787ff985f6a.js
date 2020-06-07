f46c2727d52a99feedadf5b3dcf7ed7143724955
Patrick <ppduss@gmail.com>
2020-06-07 10:58:08 -0400

    Updates

---
+++ precache-manifest.c0963acb0166fc49b9d70787ff985f6a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e4b0f97244d2a25b22733c67bd54da",
+    "url": "/ppduss/portfolio/public/index.html"
+  },
+  {
+    "revision": "5e962e698cb12182f93b",
+    "url": "/ppduss/portfolio/public/static/css/2.4fdbf1be.chunk.css"
+  },
+  {
+    "revision": "b878e01af4b355b12e32",
+    "url": "/ppduss/portfolio/public/static/css/main.67bae2a1.chunk.css"
+  },
+  {
+    "revision": "5e962e698cb12182f93b",
+    "url": "/ppduss/portfolio/public/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ppduss/portfolio/public/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b878e01af4b355b12e32",
+    "url": "/ppduss/portfolio/public/static/js/main.159713e7.chunk.js"
+  },
+  {
+    "revision": "74b54bd2fb8356597639",
+    "url": "/ppduss/portfolio/public/static/js/runtime-main.221fe1fb.js"
+  },
+  {
+    "revision": "d5363c65fc4394a4e9ef9a62bded7c99",
+    "url": "/ppduss/portfolio/public/static/media/face3.d5363c65.jpg"
+  },
+  {
+    "revision": "5f4e00a53df9e97c681768db5c456a6b",
+    "url": "/ppduss/portfolio/public/static/media/greenery.5f4e00a5.jpeg"
+  },
+  {
+    "revision": "db64222bd6ed1a3af3a2ae3788f694ce",
+    "url": "/ppduss/portfolio/public/static/media/ratatap2.db64222b.png"
+  }
+]);
