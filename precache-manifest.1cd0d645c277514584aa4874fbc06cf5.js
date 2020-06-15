0d4fce380e2b3697824706bcdb30a94323f9806b
Patrick <ppduss@gmail.com>
2020-06-15 12:15:38 -0400

    Updates

---
+++ precache-manifest.1cd0d645c277514584aa4874fbc06cf5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9854c8fcfc73fdd3e66e1e4240bcff01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e962e698cb12182f93b",
+    "url": "/static/css/2.4fdbf1be.chunk.css"
+  },
+  {
+    "revision": "60520746874e16bedd91",
+    "url": "/static/css/main.19953f7e.chunk.css"
+  },
+  {
+    "revision": "5e962e698cb12182f93b",
+    "url": "/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60520746874e16bedd91",
+    "url": "/static/js/main.e5ccbb46.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "d5363c65fc4394a4e9ef9a62bded7c99",
+    "url": "/static/media/face3.d5363c65.jpg"
+  },
+  {
+    "revision": "5f4e00a53df9e97c681768db5c456a6b",
+    "url": "/static/media/greenery.5f4e00a5.jpeg"
+  },
+  {
+    "revision": "db64222bd6ed1a3af3a2ae3788f694ce",
+    "url": "/static/media/ratatap2.db64222b.png"
+  }
+]);
